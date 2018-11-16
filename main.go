8b8427ef90e6c66e011a13127db367df1bf1b3f8
zan <zan_zibert@hotmail.com>
2018-11-16 09:16:44 +0100

    Named return values

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
-	return x + y
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
-	fmt.Println(add(42, 13))
+	fmt.Println(split(17))
 }
