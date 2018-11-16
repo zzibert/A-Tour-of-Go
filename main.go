0c6b5f8c80e3cfb395f1d47986d996a3081516cd
zan <zan_zibert@hotmail.com>
2018-11-16 09:33:25 +0100

    package main

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import "fmt"
 
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
-
 func main() {
-	fmt.Println(split(17))
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
