a6244a671cb57eae8f2bcadcbb078275aef60de4
zan <zan_zibert@hotmail.com>
2018-11-18 20:45:43 +0100

    Exercise: Stringers

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
-}
+type IPAddr [4]byte
 
-func (p Person) String() string {
-	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+// TODO: Add a "String() string" method to IPAddr.
+func (i IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", i[0], i[1], i[2], i[3])
 }
 
 func main() {
-	a := Person{"Arthur Dent", 42}
-	z := Person{"Zaphod Beeblebrox", 9001}
-	fmt.Println(a, z)
+	hosts := map[string]IPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
 }
