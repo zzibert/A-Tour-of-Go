07645ba4e9f158a37d4806ad14e114d94bb5808c
zan <zan_zibert@hotmail.com>
2018-11-19 11:47:44 +0100

    Values

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+
+	// Strings, which can be added together with `+`.
+	fmt.Println("go" + "lang")
+
+	// Integers and floats.
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// Booleans, with boolean operators as you'd expect.
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
