627f90eb3d97695008e5a6a8372fcb95948f49ed
zan <zan_zibert@hotmail.com>
2018-11-19 14:11:27 +0100

    Constants

--- main.go
+++ main.go
@@ -2,22 +2,22 @@ package main
 
 import "fmt"
 
+// `const` declares a constant value.
+const s string = "constant"
+
 func main() {
+	fmt.Println(s)
 
-    // `var` declares 1 or more variables.
-    var a = "initial"
-    fmt.Println(a)
+	// A `const` statement can appear anywhere a `var`
+	// statement can.
+	const n = 500000000
 
-    // You can declare multiple variables at once.
-    var b, c int = 1, 2
-    fmt.Println(b, c)
+	// Constant expressions perform arithmetic with
+	// arbitrary precision.
+	const d = 3e20 / n
+	fmt.Println(d)
 
-    // Go will infer the type of initialized variables.
-    var d = true
-    fmt.Println(d)
-
-    // Variables declared without a corresponding
-    // initialization are _zero-valued_. For example, the
-    // zero value for an `int` is `0`.
-    var e int
-    fmt.Println(e)
+	// A numeric constant has no type until it's given
+	// one, such as by an explicit cast.
+	fmt.Println(int64(d))
+}
