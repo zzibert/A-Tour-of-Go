25a2bdf0f45b616bcffc9ab57777220a0cad244c
zan <zan_zibert@hotmail.com>
2018-11-19 15:12:52 +0100

    If/Else

--- main.go
+++ main.go
@@ -4,32 +4,29 @@ import "fmt"
 
 func main() {
 
-	// The most basic type, with a single condition.
-	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i = i + 1
+	// Here's a basic example.
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
 	}
 
-	// A classic initial/condition/after `for` loop.
-	for j := 7; j <= 9; j++ {
-		fmt.Println(j)
+	// You can have an `if` statement without an else.
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
 	}
 
-	// `for` without a condition will loop repeatedly
-	// until you `break` out of the loop or `return` from
-	// the enclosing function.
-	for {
-		fmt.Println("loop")
-		break
-	}
-
-	// You can also `continue` to the next iteration of
-	// the loop.
-	for n := 0; n <= 5; n++ {
-		if n%2 == 0 {
-			continue
-		}
-		fmt.Println(n)
+	// A statement can precede conditionals; any variables
+	// declared in this statement are available in all
+	// branches.
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
 	}
 }
+
+// Note that you don't need parentheses around conditions
+// in Go, but that the braces are required.
