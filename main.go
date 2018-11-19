3bcd719cc1da72677807dfc0cd50cbc7c6877f89
zan <zan_zibert@hotmail.com>
2018-11-19 14:13:24 +0100

    For

--- main.go
+++ main.go
@@ -2,22 +2,34 @@ package main
 
 import "fmt"
 
-// `const` declares a constant value.
-const s string = "constant"
-
 func main() {
-	fmt.Println(s)
 
-	// A `const` statement can appear anywhere a `var`
-	// statement can.
-	const n = 500000000
+	// The most basic type, with a single condition.
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
 
-	// Constant expressions perform arithmetic with
-	// arbitrary precision.
-	const d = 3e20 / n
-	fmt.Println(d)
+	// A classic initial/condition/after `for` loop.
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
 
-	// A numeric constant has no type until it's given
-	// one, such as by an explicit cast.
-	fmt.Println(int64(d))
+	// `for` without a condition will loop repeatedly
+	// until you `break` out of the loop or `return` from
+	// the enclosing function.
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	// You can also `continue` to the next iteration of
+	// the loop.
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
 }
