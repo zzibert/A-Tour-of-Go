d1cd4013dab7fb9180a133bb8f5d450935290d26
zan <zan_zibert@hotmail.com>
2018-11-23 19:37:18 +0100

    Multiple Return Values

--- main.go
+++ main.go
@@ -1,35 +1,27 @@
-// _Functions_ are central in Go. We'll learn about
-// functions with a few different examples.
+// Go has built-in support for _multiple return values_.
+// This feature is used often in idiomatic Go, for example
+// to return both result and error values from a function.
 
 package main
 
 import "fmt"
 
-// Here's a function that takes two `int`s and returns
-// their sum as an `int`.
-func plus(a int, b int) int {
-
-	// Go requires explicit returns, i.e. it won't
-	// automatically return the value of the last
-	// expression.
-	return a + b
-}
-
-// When you have multiple consecutive parameters of
-// the same type, you may omit the type name for the
-// like-typed parameters up to the final parameter that
-// declares the type.
-func plusPlus(a, b, c int) int {
-	return a + b + c
+// The `(int, int)` in this function signature shows that
+// the function returns 2 `int`s.
+func vals() (int, int) {
+	return 3, 7
 }
 
 func main() {
 
-	// Call a function just as you'd expect, with
-	// `name(args)`.
-	res := plus(1, 2)
-	fmt.Println("1+2 =", res)
+	// Here we use the 2 different return values from the
+	// call with _multiple assignment_.
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
 
-	res = plusPlus(1, 2, 3)
-	fmt.Println("1+2+3 =", res)
+	// If you only want a subset of the returned values,
+	// use the blank identifier `_`.
+	_, c := vals()
+	fmt.Println(c)
 }
