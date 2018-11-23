e99fc7309d9f4ccabaa2e33b156b6522fd0eaa7a
zan <zan_zibert@hotmail.com>
2018-11-23 20:16:24 +0100

    Structs

--- main.go
+++ main.go
@@ -1,42 +1,41 @@
-// Go supports <em><a href="http://en.wikipedia.org/wiki/Pointer_(computer_programming)">pointers</a></em>,
-// allowing you to pass references to values and records
-// within your program.
+// Go's _structs_ are typed collections of fields.
+// They're useful for grouping data together to form
+// records.
 
 package main
 
 import "fmt"
 
-// We'll show how pointers work in contrast to values with
-// 2 functions: `zeroval` and `zeroptr`. `zeroval` has an
-// `int` parameter, so arguments will be passed to it by
-// value. `zeroval` will get a copy of `ival` distinct
-// from the one in the calling function.
-func zeroval(ival int) {
-	ival = 0
-}
-
-// `zeroptr` in contrast has an `*int` parameter, meaning
-// that it takes an `int` pointer. The `*iptr` code in the
-// function body then _dereferences_ the pointer from its
-// memory address to the current value at that address.
-// Assigning a value to a dereferenced pointer changes the
-// value at the referenced address.
-func zeroptr(iptr *int) {
-	*iptr = 0
+// This `person` struct type has `name` and `age` fields.
+type person struct {
+	name string
+	age  int
 }
 
 func main() {
-	i := 1
-	fmt.Println("initial:", i)
 
-	zeroval(i)
-	fmt.Println("zeroval:", i)
+	// This syntax creates a new struct.
+	fmt.Println(person{"Bob", 20})
 
-	// The `&i` syntax gives the memory address of `i`,
-	// i.e. a pointer to `i`.
-	zeroptr(&i)
-	fmt.Println("zeroptr:", i)
+	// You can name the fields when initializing a struct.
+	fmt.Println(person{name: "Alice", age: 30})
 
-	// Pointers can be printed too.
-	fmt.Println("pointer:", &i)
+	// Omitted fields will be zero-valued.
+	fmt.Println(person{name: "Fred"})
+
+	// An `&` prefix yields a pointer to the struct.
+	fmt.Println(&person{name: "Ann", age: 40})
+
+	// Access struct fields with a dot.
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	// You can also use dots with struct pointers - the
+	// pointers are automatically dereferenced.
+	sp := &s
+	fmt.Println(sp.age)
+
+	// Structs are mutable.
+	sp.age = 51
+	fmt.Println(sp.age)
 }
