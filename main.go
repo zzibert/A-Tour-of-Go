54fbed074c478e4395243b4758f9906b874cd762
zan <zan_zibert@hotmail.com>
2018-11-23 21:07:49 +0100

    Methods

--- main.go
+++ main.go
@@ -1,41 +1,37 @@
-// Go's _structs_ are typed collections of fields.
-// They're useful for grouping data together to form
-// records.
+// Go supports _methods_ defined on struct types.
 
 package main
 
 import "fmt"
 
-// This `person` struct type has `name` and `age` fields.
-type person struct {
-	name string
-	age  int
+type rect struct {
+	width, height int
+}
+
+// This `area` method has a _receiver type_ of `*rect`.
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+// Methods can be defined for either pointer or value
+// receiver types. Here's an example of a value receiver.
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
 }
 
 func main() {
+	r := rect{width: 10, height: 5}
 
-	// This syntax creates a new struct.
-	fmt.Println(person{"Bob", 20})
+	// Here we call the 2 methods defined for our struct.
+	fmt.Println("area: ", r.area())
+	fmt.Println("perim:", r.perim())
 
-	// You can name the fields when initializing a struct.
-	fmt.Println(person{name: "Alice", age: 30})
-
-	// Omitted fields will be zero-valued.
-	fmt.Println(person{name: "Fred"})
-
-	// An `&` prefix yields a pointer to the struct.
-	fmt.Println(&person{name: "Ann", age: 40})
-
-	// Access struct fields with a dot.
-	s := person{name: "Sean", age: 50}
-	fmt.Println(s.name)
-
-	// You can also use dots with struct pointers - the
-	// pointers are automatically dereferenced.
-	sp := &s
-	fmt.Println(sp.age)
-
-	// Structs are mutable.
-	sp.age = 51
-	fmt.Println(sp.age)
+	// Go automatically handles conversion between values
+	// and pointers for method calls. You may want to use
+	// a pointer receiver type to avoid copying on method
+	// calls or to allow the method to mutate the
+	// receiving struct.
+	rp := &r
+	fmt.Println("area: ", rp.area())
+	fmt.Println("perim:", rp.perim())
 }
