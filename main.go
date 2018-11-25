778196d75d4f4185e6bc5f4d79ab68fb5c0ad712
zan <zan_zibert@hotmail.com>
2018-11-25 18:01:46 +0100

    Worker Pools

--- main.go
+++ main.go
@@ -1,31 +1,48 @@
-// [Timers](timers) are for when you want to do
-// something once in the future - _tickers_ are for when
-// you want to do something repeatedly at regular
-// intervals. Here's an example of a ticker that ticks
-// periodically until we stop it.
+// In this example we'll look at how to implement
+// a _worker pool_ using goroutines and channels.
 
 package main
 
-import "time"
 import "fmt"
+import "time"
+
+// Here's the worker, of which we'll run several
+// concurrent instances. These workers will receive
+// work on the `jobs` channel and send the corresponding
+// results on `results`. We'll sleep a second per job to
+// simulate an expensive task.
+func worker(id int, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		fmt.Println("worker", id, "started  job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
 
 func main() {
 
-	// Tickers use a similar mechanism to timers: a
-	// channel that is sent values. Here we'll use the
-	// `range` builtin on the channel to iterate over
-	// the values as they arrive every 500ms.
-	ticker := time.NewTicker(1000 * time.Millisecond)
-	go func() {
-		for t := range ticker.C {
-			fmt.Println("Tick at", t)
-		}
-	}()
+	// In order to use our pool of workers we need to send
+	// them work and collect their results. We make 2
+	// channels for this.
+	jobs := make(chan int, 100)
+	results := make(chan int, 100)
 
-	// Tickers can be stopped like timers. Once a ticker
-	// is stopped it won't receive any more values on its
-	// channel. We'll stop ours after 1600ms.
-	time.Sleep(5000 * time.Millisecond)
-	ticker.Stop()
-	fmt.Println("Ticker stopped")
+	// This starts up 3 workers, initially blocked
+	// because there are no jobs yet.
+	for w := 1; w <= 3; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// Here we send 5 `jobs` and then `close` that
+	// channel to indicate that's all the work we have.
+	for j := 1; j <= 5; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	// Finally we collect all the results of the work.
+	for a := 1; a <= 5; a++ {
+		<-results
+	}
 }
