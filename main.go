7a8353d2a1cdc8562e96aaca1f7126853a4fc2fb
zan <zan_zibert@hotmail.com>
2018-11-25 16:05:06 +0100

    Range over Channels

--- main.go
+++ main.go
@@ -1,50 +1,25 @@
-// _Closing_ a channel indicates that no more values
-// will be sent on it. This can be useful to communicate
-// completion to the channel's receivers.
+// In a [previous](range) example we saw how `for` and
+// `range` provide iteration over basic data structures.
+// We can also use this syntax to iterate over
+// values received from a channel.
 
 package main
 
 import "fmt"
 
-// In this example we'll use a `jobs` channel to
-// communicate work to be done from the `main()` goroutine
-// to a worker goroutine. When we have no more jobs for
-// the worker we'll `close` the `jobs` channel.
 func main() {
-	jobs := make(chan int, 5)
-	done := make(chan bool)
 
-	// Here's the worker goroutine. It repeatedly receives
-	// from `jobs` with `j, more := <-jobs`. In this
-	// special 2-value form of receive, the `more` value
-	// will be `false` if `jobs` has been `close`d and all
-	// values in the channel have already been received.
-	// We use this to notify on `done` when we've worked
-	// all our jobs.
-	go func() {
-		for {
-			j, more := <-jobs
-			if more {
-				fmt.Println("received job", j)
-			} else {
-				fmt.Println("received all jobs")
-				done <- true
-				return
-			}
-		}
-	}()
+	// We'll iterate over 2 values in the `queue` channel.
+	queue := make(chan string, 2)
+	queue <- "one"
+	queue <- "two"
+	close(queue)
 
-	// This sends 3 jobs to the worker over the `jobs`
-	// channel, then closes it.
-	for j := 1; j <= 3; j++ {
-		jobs <- j
-		fmt.Println("sent job", j)
+	// This `range` iterates over each element as it's
+	// received from `queue`. Because we `close`d the
+	// channel above, the iteration terminates after
+	// receiving the 2 elements.
+	for elem := range queue {
+		fmt.Println(elem)
 	}
-	close(jobs)
-	fmt.Println("sent all jobs")
-
-	// We await the worker using the
-	// [synchronization](channel-synchronization) approach
-	// we saw earlier.
-	<-done
 }
