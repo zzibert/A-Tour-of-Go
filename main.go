0a2183597d4c016aef9b6e784a1290a3f37f7409
zan <zan_zibert@hotmail.com>
2018-11-28 21:42:12 +0100

    Type inference

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	var z uint = uint(f)
-	fmt.Println(x, y, z)
+	v := 42.02 + 0.5i
+	fmt.Printf("v is of type %T\n", v)
 }
