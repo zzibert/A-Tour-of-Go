c43506210f8f7758d9730113f6c9ee55cb8af592
zan <zan_zibert@hotmail.com>
2018-11-29 22:02:26 +0100

    Defer

--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("Good afternoon.")
-	default:
-		fmt.Println("Good evening.")
-	}
+	defer fmt.Println("world")
+
+	fmt.Println("hello")
 }
