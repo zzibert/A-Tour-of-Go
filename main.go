bbc2c930888a76a9a9cb6e25c218b1dd9239b789
zan <zan_zibert@hotmail.com>
2018-12-22 12:29:29 +0100

    Packages

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
-func sortArrayByParity(A []int) []int {
-	even := make([]int, 0)
-	odd := make([]int, 0)
-	for _, v := range A {
-		if v%2 == 0 {
-			even = append(even, v)
-		} else {
-			odd = append(odd, v)
-		}
-	}
-	return append(even, odd...)
-}
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
