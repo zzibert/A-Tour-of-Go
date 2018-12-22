7fb4ea517a8279db7f5575e76b3eda05bab15e22
zan <zan_zibert@hotmail.com>
2018-12-22 17:11:11 +0100

    Struct Literals

--- main.go
+++ main.go
@@ -3,13 +3,16 @@ package main
 import "fmt"
 
 type Vertex struct {
-	X int
-	Y int
+	X, Y int
 }
 
+var (
+	v1 = Vertex{1, 2}  // has type Vertex
+	v2 = Vertex{X: 1}  // Y:0 is implicit
+	v3 = Vertex{}      // X:0 and Y:0
+	p  = &Vertex{1, 2} // has type *Vertex
+)
+
 func main() {
-	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9
-	fmt.Println(v)
+	fmt.Println(v1, p, v2, v3)
 }
