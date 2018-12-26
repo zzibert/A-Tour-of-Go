db10934ae750755a7bc0614c29dafa9c05dc488f
zan <zan_zibert@hotmail.com>
2018-12-26 13:23:11 +0100

    Swords and guns

--- main.go
+++ main.go
@@ -2,45 +2,35 @@ package main
 
 import "fmt"
 
-type power struct {
-	attack  int
-	defense int
+type attacker struct {
+	attackPower int
+	damageBonus int
 }
 
-type location struct {
-	x float32
-	y float32
-	z float32
+type sword struct {
+	attacker
+	twoHanded bool
 }
 
-type nonPlayerCharacter struct {
-	name  string
-	speed int
-	hp    int
-	power power
-	loc   location
+type gun struct {
+	attacker
+	bullletsRemaining int
+}
+
+func (s sword) Wield() bool {
+	fmt.Println("You've wielded a sword!")
+	return true
+}
+
+func (g gun) Wield() bool {
+	fmt.Println("You've wielded a gun!")
+	return true
 }
 
 func main() {
-	fmt.Println("Structs...")
-
-	demon := nonPlayerCharacter{
-		name:  "Alfred",
-		speed: 21,
-		hp:    100,
-		power: power{attack: 75, defense: 50},
-		loc:   location{x: 1075.123, y: 521.123, z: 211.231},
-	}
-
-	fmt.Println(demon)
-
-	anotherDemon := nonPlayerCharacter{
-		name:  "Beelzebub",
-		speed: 30,
-		hp:    5000,
-		power: power{attack: 10, defense: 10},
-		loc:   location{x: 32.03, y: 72.45, z: 65.231},
-	}
-
-	fmt.Println(anotherDemon)
+	sword1 := sword{attacker: attacker{attackPower: 1, damageBonus: 5}, twoHanded: true}
+	gun1 := gun{attacker: attacker{attackPower: 10, damageBonus: 20}, bullletsRemaining: 11}
+	fmt.Printf("Weapons: sword: %v, gun: %v\n", sword1, gun1)
+	sword1.Wield()
+	gun1.Wield()
 }
