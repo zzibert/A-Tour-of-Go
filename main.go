9309931e0ee94553d3cb564e6c6486b47bf0660d
zan <zan_zibert@hotmail.com>
2018-12-26 13:03:14 +0100

    demon structs

--- main.go
+++ main.go
@@ -1,36 +1,46 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-func fakeDieRoll(size int) int {
-	return 42
+type power struct {
+	attack  int
+	defense int
 }
 
-func getDieRolls() []dieRollFunc {
-	return []dieRollFunc{
-		dieRoll,
-		fakeDieRoll,
-	}
+type location struct {
+	x float32
+	y float32
+	z float32
 }
 
-type dieRollFunc func(int) int
-
-func dieRoll(size int) int {
-	rand.Seed(time.Now().UnixNano())
-	return rand.Intn(size) + 1
-}
-
-func rollTwo(size1, size2 int) (int, int) {
-	return dieRoll(size1), dieRoll(size2)
+type nonPlayerCharacter struct {
+	name  string
+	speed int
+	hp    int
+	power power
+	loc   location
 }
 
 func main() {
-	rolls := getDieRolls()
-	for index, rollFunc := range rolls {
-		fmt.Printf("Die Roll Attempt #%d, result: %d\n", index, rollFunc(10))
+	fmt.Println("Structs...")
+
+	demon := nonPlayerCharacter{
+		name:  "Alfred",
+		speed: 21,
+		hp:    100,
+		power: power{attack: 75, defense: 50},
+		loc:   location{x: 1075.123, y: 521.123, z: 211.231},
 	}
+
+	fmt.Println(demon)
+
+	anotherDemon := nonPlayerCharacter{
+		name:  "Beelzebub",
+		speed: 30,
+		hp:    5000,
+		power: power{attack: 10, defense: 10},
+		loc:   location{x: 32.03, y: 72.45, z: 65.231},
+	}
+
+	fmt.Println(anotherDemon)
 }
