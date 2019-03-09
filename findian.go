d790f6897aeb22d5bbc76eca3e883aa2bcfd4728
zan <zan_zibert@hotmail.com>
2019-03-09 19:48:57 +0100

    week 3 exercise 1

--- findian.go
+++ findian.go
@@ -6,12 +6,8 @@ import (
 )
 
 func main() {
-	var x string
-	fmt.Println("Enter a string in double quotes.")
-	fmt.Scanf("%q", &x)
-	if strings.ContainsAny(x, "aA") && (x[0] == 'i' || x[0] == 'I') && (x[len(x)-1] == 'n' || x[len(x)-1] == 'N') {
-		fmt.Println("Found!")
-	} else {
-		fmt.Println("Not found!")
+	x := [5]int {1, 2, 3, 4, 5}
+	for i, v range x {
+		fmt.Println("index %d value %d", i, v)
 	}
 }
