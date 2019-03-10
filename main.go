7484a30d666246ac8f370f68bb8e90010b2926e8
zan <zan_zibert@hotmail.com>
2019-03-10 19:53:58 +0100

    hello world!

--- main.go
+++ main.go
@@ -2,14 +2,6 @@ package main
 
 import "fmt"
 
-func foo() *int {
-	x := 1
-	return &x
-}
-
 func main() {
-	var y *int
-	y = foo()
-	fmt.Printf("Hi %s\n", "Joe")
-	fmt.Printf("%d", *y)
+	fmt.Println("Hello, 世界")
 }
