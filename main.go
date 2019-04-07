2b4b6838f30b0ca1734649bd19e0d847c7b7f84e
zan <zan_zibert@hotmail.com>
2019-04-07 11:59:19 +0200

    philosopher chopsticks

--- main.go
+++ main.go
@@ -1,23 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 )
 
-var i = 0
-var wg sync.WaitGroup
-var once sync.Once
-
-func main() {
-	var c1, c2 chan int
-	wg.Add(2)
-	go doStuff(c1, c2)
-	go doStuff(c2, c1)
-	wg.Wait()
+type ChopS struct {
+	sync.Mutex
 }
 
-func doStuff(c1 chan int, c2 chan int) {
-	<-c1
-	c2 <- 1
-	wg.Done()
+type Philo struct {
+	leftCS, rightCS *ChopS
+}
+
+func (p Philo) eat() {
+	for {
+		p.leftCS.lock()
+		p.rightCS.lock()
+
+		fmt.Println("eating")
+
+		p.rightCS.Unlock()
+		p.leftCS.Unlock()
+	}
+}
+
+func main() {
+	CSticks := make([]*ChopS, 5)
+	for i := 0; i < 5; i++ {
+		CSticks[i] = new(ChopS)
+	}
+	philos := make([]*Philo, 5)
+	for i := 0; i < 5; i++ {
+		philos[i] = &Philo{CSticks[i], CSticks[(i+1)%5]}
+	}
+	for i := 0; i < 5; i++ {
+		go philos[i].eat()
+	}
 }
