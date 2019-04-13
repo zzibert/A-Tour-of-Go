cbbbecf641b02a455b973a2abf1371dfdf5a88d7
zan <zan_zibert@hotmail.com>
2019-04-13 10:00:02 +0200

    first version made

--- quiz-game.go
+++ quiz-game.go
@@ -4,9 +4,13 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
+	//Initializing counter
+	counter := 0
+
 	//Opening the file
 	f, err := os.Open("problems.csv")
 	if err != nil {
@@ -16,11 +20,25 @@ func main() {
 
 	//Reading the file
 	lines, err := csv.NewReader(f).ReadAll()
+
+	//Initializing max score
+	maxScore := len(lines)
+
 	if err != nil {
 		fmt.Println(err)
 	}
+	answer := 0
 	for _, line := range lines {
-		fmt.Println(line[0])
-	}
+		fmt.Printf("What is the result of %s: ", line[0])
 
+		_, err := fmt.Scanf("%d\n", &answer)
+		if err != nil {
+			fmt.Println(err)
+		}
+		i, err := strconv.Atoi(line[1])
+		if answer == i {
+			counter++
+		}
+	}
+	fmt.Printf("Your test score is %d out of %d", counter, maxScore)
 }
