277e9b174ac9f416b49120541cbe00b929f41ff1
zan <zan_zibert@hotmail.com>
2019-04-13 09:46:16 +0200

    Reading the csv file

--- quiz-game.go
+++ quiz-game.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
 )
 
 func main() {
+	//Opening the file
 	f, err := os.Open("problems.csv")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer f.Close()
+
+	//Reading the file
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, line := range lines {
+		fmt.Println(line[0])
+	}
+
 }
