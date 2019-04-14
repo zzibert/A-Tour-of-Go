99fa7060ae07cc82f4739f773ac27a1f87a53621
zan <zan_zibert@hotmail.com>
2019-04-14 16:25:27 +0200

    accessing the internet in go

--- exercise #3/adventure.go
+++ exercise #3/adventure.go
@@ -2,21 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
-	fmt.Fprintf(w, "<p>Go is fast!</p>")
-	fmt.Fprintf(w, "<p>..and Go is simple!</p>")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>This is the golang about page</h1>")
-}
-
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.ListenAndServe(":8000", nil)
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.html")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	string_body := string(bytes)
+	fmt.Println(string_body)
+	resp.Body.Close()
 }
