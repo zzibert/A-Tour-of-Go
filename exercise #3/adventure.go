641c87b36fe1c09b78cea6513af8fc25cb1f3b7e
zan <zan_zibert@hotmail.com>
2019-04-14 16:33:56 +0200

    xml parsing

--- exercise #3/adventure.go
+++ exercise #3/adventure.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:loc`
+}
 
 func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.html")
@@ -12,4 +21,9 @@ func main() {
 	string_body := string(bytes)
 	fmt.Println(string_body)
 	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
 }
