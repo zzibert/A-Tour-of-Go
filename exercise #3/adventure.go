52d7bf8fc89b8e52b80ae35e4bd796673218800b
zan <zan_zibert@hotmail.com>
2019-04-14 13:20:56 +0200

    adding a few lines

--- exercise #3/adventure.go
+++ exercise #3/adventure.go
@@ -7,6 +7,8 @@ import (
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+	fmt.Fprintf(w, "<p>Go is fast!</p>")
+	fmt.Fprintf(w, "<p>..and Go is simple!</p>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
