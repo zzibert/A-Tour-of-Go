b4fb1d1f304ae1162d8923aa3943b14be89518bf
zan <zan_zibert@hotmail.com>
2019-04-14 21:25:06 +0200

    figuring out exercise #3

--- exercise #3/adventure.go
+++ exercise #3/adventure.go
@@ -1,28 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"net/http"
-)
+// func indexHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+// }
 
-type NewsAggPage struct {
-	Title string
-	News  string
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
-}
-
-func NewsAggHandler(w http.ResponseWriter, r *http.Request) {
-	p := NewsAggPage{Title: "Amazing news Aggregator", News: "trololo trololo trololo"}
-	t, _ := template.ParseFiles("basicTemplating.html")
-	t.Execute(w, p)
+// func NewsAggHandler(w http.ResponseWriter, r *http.Request) {
+// 	p := NewsAggPage{Title: "Amazing news Aggregator", News: "trololo trololo trololo"}
+// 	t, _ := template.ParseFiles("basicTemplating.html")
+// 	t.Execute(w, p)
+// }
+type Chapter struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/agg/", NewsAggHandler)
-	http.ListenAndServe(":8000", nil)
+	file, _ := ioutil.ReadFile("./gopher.json")
+	chapters := new(map[string]Chapter)
+
+	if err := json.Unmarshal(file, &chapters)
+
+	// for _, chapter := range chapters {
+	// 	fmt.Println(chapter.Title)
+	// }
+
+	// http.HandleFunc("/", indexHandler)
+	// http.HandleFunc("/agg/", NewsAggHandler)
+	// http.ListenAndServe(":8000", nil)
 }
