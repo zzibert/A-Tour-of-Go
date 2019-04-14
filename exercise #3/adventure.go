7cbe3d3b0fc8059953fab08a91e1e439858f2c64
zan <zan_zibert@hotmail.com>
2019-04-14 13:00:56 +0200

    adding the about route

--- exercise #3/adventure.go
+++ exercise #3/adventure.go
@@ -9,7 +9,12 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>This is the golang about page</h1>")
+}
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/about", aboutHandler)
 	http.ListenAndServe(":8000", nil)
 }
