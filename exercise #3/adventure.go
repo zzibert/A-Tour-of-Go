c7d515a1840f8f33bf83e1895891fe263e6b0480
zan <zan_zibert@hotmail.com>
2019-04-15 21:27:53 +0200

    created all routeHandlers

--- exercise #3/adventure.go
+++ exercise #3/adventure.go
@@ -3,22 +3,25 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 )
 
-// func indexHandler(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
-// }
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
+}
 
-// func NewsAggHandler(w http.ResponseWriter, r *http.Request) {
-// 	p := NewsAggPage{Title: "Amazing news Aggregator", News: "trololo trololo trololo"}
-// 	t, _ := template.ParseFiles("basicTemplating.html")
-// 	t.Execute(w, p)
-// }
+func NewsAggHandler(w http.ResponseWriter, r *http.Request) {
+	p := NewsAggPage{Title: "Amazing news Aggregator", News: "trololo trololo trololo"}
+	t, _ := template.ParseFiles("basicTemplating.html")
+	t.Execute(w, p)
+}
+
 type Chapter struct {
-	Title   string   `json:"title"`
-	Story   []string `json:"story"`
-	Options []struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []struct {
 		Text string `json:"text"`
 		Arc  string `json:"arc"`
 	} `json:"options"`
@@ -36,9 +39,16 @@ func main() {
 
 	for key, value := range chapters {
 		fmt.Printf("%s is for %s\n", key, value.Title)
+
 	}
 
-	// http.HandleFunc("/", indexHandler)
-	// http.HandleFunc("/agg/", NewsAggHandler)
-	// http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/", introHandler)
+	http.HandleFunc("/new-york", newYorkHandler)
+	http.HandleFunc("/debate", debatekHandler)
+	http.HandleFunc("/sean-kelly", seanKellyHandler)
+	http.HandleFunc("/mark-bates", markBatesHandler)
+	http.HandleFunc("/denver", denverHandler)
+	http.HandleFunc("/home", homeHandler)
+
+	http.ListenAndServe(":8000", nil)
 }
