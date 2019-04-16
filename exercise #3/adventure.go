4f784907fa3dc50d9ca97daeab9b43fe77137ba9
zan <zan_zibert@hotmail.com>
2019-04-16 22:35:33 +0200

    failed building a handler

--- exercise #3/adventure.go
+++ exercise #3/adventure.go
@@ -18,6 +18,13 @@ func introHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, p)
 }
 
+func Handler(url string, page Page) http.Handler {
+	return http.HandleFunc(url, func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("gopher.html")
+		t.Execute(w, page)
+	})
+}
+
 type Chapter struct {
 	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
