c939d4317f4f308efe5d75287b56dd254ed43949
zan <zan_zibert@hotmail.com>
2019-04-22 16:20:02 +0200

    Can now parse href attribute

--- HTML_Link_Parser/Link/parse.go
+++ HTML_Link_Parser/Link/parse.go
@@ -1,7 +1,6 @@
 package link
 
 import (
-	"fmt"
 	"io"
 
 	"golang.org/x/net/html"
@@ -16,22 +15,30 @@ type Link struct {
 // Parse will take an HTML document and return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
 	doc, err := html.Parse(r)
+	links := make([]Link, 0)
 	if err != nil {
 		return nil, err
 	}
-	dfs(doc)
-	return nil, nil
+	dfs(doc, &links)
+	return links, nil
 }
 
-func dfs(n *html.Node) {
+func dfs(n *html.Node, links *[]Link) {
 	if n.Type == html.ElementNode && n.Data == "a" {
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			if c.Type == html.TextNode {
-				fmt.Println(c.Data)
+		newLink := Link{"", ""}
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				newLink.Href = a.Val
 			}
 		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if c.Type == html.TextNode {
+				newLink.Text = newLink.Text + c.Data
+			}
+		}
+		*links = append(*links, newLink)
 	}
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c)
+		dfs(c, links)
 	}
 }
