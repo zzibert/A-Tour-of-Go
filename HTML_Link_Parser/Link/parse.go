ce38b77108bbd40e60295a78e266e03a5cb0905d
zan <zan_zibert@hotmail.com>
2019-04-22 17:11:58 +0200

    Created GetText function

--- HTML_Link_Parser/Link/parse.go
+++ HTML_Link_Parser/Link/parse.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -23,18 +24,30 @@ func Parse(r io.Reader) ([]Link, error) {
 	return links, nil
 }
 
+func getText(newLink *Link, n *html.Node) {
+	if n.Type == html.TextNode {
+		newLink.Text = newLink.Text + strings.TrimSpace(n.Data)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			newLink.Text = newLink.Text + strings.TrimSpace(c.Data)
+		} else {
+			getText(newLink, c)
+		}
+	}
+}
+
 func dfs(n *html.Node, links *[]Link) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		newLink := Link{"", ""}
 		for _, a := range n.Attr {
 			if a.Key == "href" {
 				newLink.Href = a.Val
+				break
 			}
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			if c.Type == html.TextNode {
-				newLink.Text = newLink.Text + c.Data
-			}
+			getText(&newLink, c)
 		}
 		*links = append(*links, newLink)
 	}
