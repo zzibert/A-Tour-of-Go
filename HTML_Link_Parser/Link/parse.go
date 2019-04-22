7fac91d9ce57f0aa6b383dbcffb070e899f502aa
zan <zan_zibert@hotmail.com>
2019-04-22 15:54:31 +0200

    Implemented the html.node dfs algorithm

--- HTML_Link_Parser/Link/parse.go
+++ HTML_Link_Parser/Link/parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 //	Link respresent a Link in a HTML document.
 type Link struct {
@@ -10,5 +15,23 @@ type Link struct {
 
 // Parse will take an HTML document and return a slice of links parsed from it.
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc)
+	return nil, nil
+}
 
+func dfs(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if c.Type == html.TextNode {
+				fmt.Println(c.Data)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c)
+	}
 }
