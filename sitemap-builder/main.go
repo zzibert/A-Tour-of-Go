8aedd33d13224e79ac6d3023c1173b475f6d75e8
zan <zan_zibert@hotmail.com>
2019-04-27 12:31:53 +0200

    succesfully links parses wikipedia

--- sitemap-builder/main.go
+++ sitemap-builder/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"../HTML_Link_Parser/Link"
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	link "github.com/zzibert/A-Tour-of-Go/HTML_Link_Parser/Link"
 )
 
 func main() {
+	//	Creating the url flag
+	urlPtr := flag.String("url", "https://en.wikipedia.org/wiki/Main_Page", "The website url")
+	flag.Parse()
+
+	response, err := http.Get(*urlPtr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	r := bytes.NewReader(body)
 	links, err := link.Parse(r)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v\n", links)
 }
