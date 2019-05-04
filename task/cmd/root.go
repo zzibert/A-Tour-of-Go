88c26b74c415e9148f8de0c01a926da45c65e283
zan <zan_zibert@hotmail.com>
2019-05-04 13:22:26 +0200

    starting from beginning

--- task/cmd/root.go
+++ task/cmd/root.go
@@ -1,10 +1,8 @@
 package cmd
 
-import (
-	"github.com/spf13/cobra"
-)
+import "github.com/spf13/cobra"
 
 var RootCmd = &cobra.Command{
-	Use:   "task",
+	Use: "task",
 	Short: "Task is a CLI task manager",
 }
