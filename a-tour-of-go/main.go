00ceb0348bd5bc41410f0c51d3610342cf684022
zan <zan_zibert@hotmail.com>
2020-06-28 14:06:26 +0200

    Stacking defers

--- a-tour-of-go/main.go
+++ a-tour-of-go/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("world")
+	fmt.Println("counting")
 
-	fmt.Println("hello")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
