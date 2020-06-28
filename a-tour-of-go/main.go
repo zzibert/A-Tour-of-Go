5c115d61e90b8483b8dec7fd0dece0b30306b688
zan <zan_zibert@hotmail.com>
2020-06-28 16:54:40 +0200

    Exercise: Fibonacci closure

--- a-tour-of-go/main.go
+++ a-tour-of-go/main.go
@@ -2,20 +2,21 @@ package main
 
 import "fmt"
 
-func adder() func(int) int {
-	sum := 0
-	return func(x int) int {
-		sum += x
-		return sum
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	fib1, fib2 := 1, 1
+	return func() int {
+		temp := fib2
+		fib2 = fib2 + fib1
+		fib1 = temp
+		return fib2
 	}
 }
 
 func main() {
-	pos, neg := adder(), adder()
+	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(
-			pos(i),
-			neg(-2*i),
-		)
+		fmt.Println(f())
 	}
 }
