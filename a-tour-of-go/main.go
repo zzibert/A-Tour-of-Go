afab0262c04806b6eba277ae79b3db0393d8d85b
zan <zan_zibert@hotmail.com>
2020-06-28 15:21:35 +0200

    Arrays

--- a-tour-of-go/main.go
+++ a-tour-of-go/main.go
@@ -2,17 +2,13 @@ package main
 
 import "fmt"
 
-type Vertex struct {
-	X, Y int
-}
-
-var (
-	v1 = Vertex{1, 2}  // has type Vertex
-	v2 = Vertex{X: 1}  // Y:0 is implicit
-	v3 = Vertex{}      // X:0 and Y:0
-	p  = &Vertex{1, 2} // has type *Vertex
-)
-
 func main() {
-	fmt.Println(v1, p, v2, v3)
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
 }
