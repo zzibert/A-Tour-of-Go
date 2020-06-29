0d8629d8e86fdce38aa498c12f1788457fc4f46f
Žan Žiber <zan.zibert@ulu.io>
2020-06-29 14:47:09 +0200

    alternative version of reverse string

--- master-the-coding-interview/arrays/reverse_string.go
+++ master-the-coding-interview/arrays/reverse_string.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Println(reverse("Hi My name is Andrei"))
+	fmt.Println(reverse2("Hi My name is Andrei"))
 }
 
 // This function has time complexity of O(n)
@@ -17,6 +18,17 @@ func reverse(str string) string {
 	for i, _ := range chars {
 		j := (len(chars) - i) - 1
 		newString = append(newString, chars[j])
+	}
+
+	return strings.Join(newString, "")
+}
+
+// Alternative version of reverse string , much simpler
+func reverse2(str string) string {
+	chars := strings.Split(str, "")
+	var newString []string
+	for i := len(chars) - 1; i >= 0; i-- {
+		newString = append(newString, chars[i])
 	}
 
 	return strings.Join(newString, "")
