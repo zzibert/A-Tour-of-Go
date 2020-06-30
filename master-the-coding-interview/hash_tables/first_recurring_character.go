c58f4d0c7b1981463d31779310f9562851a01336
Žan Žiber <zan.zibert@ulu.io>
2020-06-30 16:09:03 +0200

    completed first recurring character exercise

---
+++ master-the-coding-interview/hash_tables/first_recurring_character.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(firstReccuringCharacter([]int{2, 5, 1, 2, 3, 5, 1, 2, 4}))
+	fmt.Println(firstReccuringCharacter([]int{2, 1, 1, 2, 3, 5, 1, 2, 4}))
+	fmt.Println(firstReccuringCharacter([]int{2, 3, 4, 5}))
+}
+
+func firstReccuringCharacter(nums []int) int {
+	characters := make(map[int]bool)
+
+	for _, number := range nums {
+		if _, ok := characters[number]; ok {
+			return number
+		} else {
+			characters[number] = true
+		}
+	}
+
+	return 0
+}
