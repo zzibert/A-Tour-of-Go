237047e0f25af27b077e36945ddd69e82329d505
Žan Žiber <zan.zibert@ulu.io>
2020-07-03 16:01:26 +0200

    implementing Prepend to linkedList

--- master-the-coding-interview/linked_lists/linkedlist.go
+++ master-the-coding-interview/linked_lists/linkedlist.go
@@ -14,15 +14,19 @@ type LinkedList struct {
 
 func main() {
 	linkedList := &LinkedList{&node{value: 1, next: nil}, 1}
-	linkedList.append(2)
-	linkedList.append(3)
-	linkedList.append(4)
-	linkedList.append(5)
+	linkedList.Append(2)
+	linkedList.Append(3)
+	linkedList.Append(4)
+	linkedList.Append(5)
+	linkedList.Prepend(0)
+	linkedList.Prepend(-1)
+	linkedList.Prepend(-2)
+	linkedList.Prepend(-3)
 
 	printLinkedList(linkedList.root)
 }
 
-func (list *LinkedList) append(value interface{}) {
+func (list *LinkedList) Append(value interface{}) {
 
 	current := list.root
 
@@ -34,6 +38,11 @@ func (list *LinkedList) append(value interface{}) {
 		next:  nil,
 	}
 	current.next = newNode
+}
+
+func (list *LinkedList) Prepend(value interface{}) {
+	newNode := &node{value, list.root}
+	list.root = newNode
 }
 
 func printLinkedList(current *node) {
