9a485f2c4afcee14ab9ffd89ecee55028c300e3c
Žan Žiber <zan.zibert@ulu.io>
2020-07-07 16:18:30 +0200

    completed insertion sort

--- master-the-coding-interview/algorithms/sorting.go
+++ master-the-coding-interview/algorithms/sorting.go
@@ -3,11 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{6, 5, 8, 2, 1, 7, 3, 4, 9}
+	// 6, 5, 8, 2, 1, 7, 3, 4, 9
+	nums := []int{6, 5, 3, 1, 8, 7, 2, 4, 9}
 
 	// fmt.Println(bubbleSort(nums))
 
-	fmt.Println(SelectionSort(nums))
+	fmt.Println(InsertionSort(nums))
+}
+
+func InsertionSort(numbers []int) []int {
+	for i := 0; i < len(numbers); i++ {
+		temp := numbers[i]
+		for j := i - 1; j >= 0; j-- {
+			if numbers[j] > temp {
+				numbers[j+1] = numbers[j]
+				if j == 0 {
+					numbers[0] = temp
+				}
+			} else {
+				numbers[j+1] = temp
+				break
+			}
+
+		}
+	}
+	return numbers
 }
 
 func SelectionSort(numbers []int) []int {
@@ -26,7 +46,7 @@ func SelectionSort(numbers []int) []int {
 	return numbers
 }
 
-func bubbleSort(numbers []int) []int {
+func BubbleSort(numbers []int) []int {
 	for {
 		for i := 0; i < len(numbers)-1; i++ {
 			if numbers[i] > numbers[i+1] {
