59c01c841d7766b61deb58d9b31f7ba2178792ac
Žan Žiber <zan.zibert@ulu.io>
2020-07-07 14:44:54 +0200

    implemented selection sort

--- master-the-coding-interview/algorithms/sorting.go
+++ master-the-coding-interview/algorithms/sorting.go
@@ -5,7 +5,25 @@ import "fmt"
 func main() {
 	nums := []int{6, 5, 8, 2, 1, 7, 3, 4, 9}
 
-	fmt.Println(bubbleSort(nums))
+	// fmt.Println(bubbleSort(nums))
+
+	fmt.Println(SelectionSort(nums))
+}
+
+func SelectionSort(numbers []int) []int {
+	for i := 0; i < len(numbers); i++ {
+		min := numbers[i]
+		index := i
+		for j := i + 1; j < len(numbers); j++ {
+			if min > numbers[j] {
+				min = numbers[j]
+				index = j
+			}
+		}
+		numbers[index] = numbers[i]
+		numbers[i] = min
+	}
+	return numbers
 }
 
 func bubbleSort(numbers []int) []int {
