bc2f6f502056c6f35b3b3c7c1462d322f4635aa2
Žan Žiber <zan.zibert@ulu.io>
2020-07-08 11:59:40 +0200

    implemented is Symmetric tree with recursion 100%

--- leetcode/101_symmetric_tree/main.go
+++ leetcode/101_symmetric_tree/main.go
@@ -11,81 +11,26 @@ type TreeNode struct {
 }
 
 func isSymmetric(root *TreeNode) bool {
-	queue := make([]*TreeNode, 0)
-	counter := 1
-	level := make([]interface{}, 0)
-
 	if root == nil {
 		return true
 	}
 
-	queue = append(queue, root)
-
-	for {
-		level = make([]interface{}, 0)
-
-		for i := 0; i < counter; i++ {
-			current := queue[0]
-
-			// If current is nil
-			if current == nil {
-				queue = append(queue, nil)
-				queue = append(queue, nil)
-
-				level = append(level, nil)
-				level = append(level, nil)
-			} else {
-				// left node
-				queue = append(queue, current.Left)
-				if current.Left == nil {
-					level = append(level, nil)
-				} else {
-					level = append(level, current.Left.Val)
-				}
-
-				// right node
-
-				queue = append(queue, current.Right)
-				if current.Right == nil {
-					level = append(level, nil)
-				} else {
-					level = append(level, current.Right.Val)
-				}
-			}
-			queue = queue[1:]
-		}
-
-		if !isPalindrome(level) {
-			return false
-		}
-
-		if isNilArray(level) {
-			return true
-		}
-
-		counter = counter * 2
-	}
+	return visit(root.Left, root.Right)
 }
 
-func isNilArray(array []interface{}) bool {
-	length := len(array)
-
-	for i := 0; i < length; i++ {
-		if array[i] != nil {
-			return false
-		}
-	}
-	return true
-}
-
-func isPalindrome(array []interface{}) bool {
-	length := len(array)
-
-	for i := 0; i < length/2; i++ {
-		if array[i] != array[(length-1)-i] {
-			return false
-		}
+func visit(root1, root2 *TreeNode) bool {
+	if root1 == nil && root2 == nil {
+		return true
 	}
 
-	return true
+	if root1 == nil || root2 == nil {
+		return false
+	}
+
+	if root1.Val != root2.Val {
+		return false
+	}
+
+	return visit(root1.Left, root2.Right) && visit(root1.Right, root2.Left)
+
 }
