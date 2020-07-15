3131bc23f131e6e7772eacb418640ce459abfebb
Zan Zibert <zan_zibert@hotmail.com>
2020-07-15 12:57:39 +0200

    completed 811 subdomain visit count

--- leetcode/811_Subdomain_Visit_Count/main.go
+++ leetcode/811_Subdomain_Visit_Count/main.go
@@ -1,6 +1,39 @@
 package main
 
-// first thing in the morning.
-func subdomainVisits(cpdomains []string) []string {
+import (
+	"strconv"
+	"strings"
+)
 
+func subdomainVisits(cpdomains []string) []string {
+	domains := make(map[string]int)
+
+	for _, cpdomain := range cpdomains {
+		count, domain := strings.Split(cpdomain, " ")[0], strings.Split(cpdomain, " ")[1]
+		number, _ := strconv.Atoi(count)
+		countSubDomains(domain, domains, number)
+	}
+
+	cpdomains = []string{}
+	for domain, count := range domains {
+		cpdomain := strconv.Itoa(count) + " " + domain
+		cpdomains = append(cpdomains, cpdomain)
+	}
+
+	return cpdomains
+}
+
+func countSubDomains(domain string, domains map[string]int, count int) {
+	subdomains := strings.Split(domain, ".")
+	for i := 0; i < len(subdomains); i++ {
+		subdomain := ""
+		for j := i; j < len(subdomains); j++ {
+			if j == len(subdomains)-1 {
+				subdomain += subdomains[j]
+			} else {
+				subdomain += subdomains[j] + "."
+			}
+		}
+		domains[subdomain] += count
+	}
 }
