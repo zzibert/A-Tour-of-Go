3058e25d04ab31f92b38446fa97126901436ba31
Zan Zibert <zan_zibert@hotmail.com>
2020-07-15 11:57:00 +0200

    Completed 339 nested list weight sum

--- leetcode/339_Nested_List_Weight_Sum/main.go
+++ leetcode/339_Nested_List_Weight_Sum/main.go
@@ -1,9 +1,19 @@
 package main
 
 func depthSum(nestedList []*NestedInteger) int {
-    
+
+	return calculateSum(nestedList, 1)
 }
 
-func calculateSum(array, depth) {
-  
+func calculateSum(array []*NestedInteger, depth int) int {
+	sum := 0
+	for _, number := range array {
+		if number.IsInteger() {
+			sum += number.GetInteger() * depth
+		} else {
+			sum += calculateSum(number.GetList(), depth+1)
+		}
+	}
+
+	return sum
 }
