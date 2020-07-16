58f9b2b3760aa92051e8bf02855661f0ca098edb
Zan Zibert <zan_zibert@hotmail.com>
2020-07-16 14:09:52 +0200

    completed 733 flood fill, dfs approach

--- leetcode/733_Flood_Fill/main.go
+++ leetcode/733_Flood_Fill/main.go
@@ -1,5 +1,27 @@
 package main
 
 func floodFill(image [][]int, sr int, sc int, newColor int) [][]int {
+	oldColor := image[sr][sc]
 
+	if oldColor == newColor {
+		return image
+	}
+
+	hasSameColor(image, sr, sc, oldColor, newColor)
+
+	return image
+}
+
+func hasSameColor(image [][]int, pixelRow, pixelColumn, oldColor, newColor int) {
+	if pixelRow >= 0 && pixelRow < len(image) && pixelColumn >= 0 && pixelColumn < len(image[pixelRow]) && image[pixelRow][pixelColumn] == oldColor {
+		image[pixelRow][pixelColumn] = newColor
+
+		hasSameColor(image, pixelRow-1, pixelColumn, oldColor, newColor)
+
+		hasSameColor(image, pixelRow, pixelColumn-1, oldColor, newColor)
+
+		hasSameColor(image, pixelRow+1, pixelColumn, oldColor, newColor)
+
+		hasSameColor(image, pixelRow, pixelColumn+1, oldColor, newColor)
+	}
 }
