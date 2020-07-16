fe1ee7beef754695bd2991525e3d896a6fc418be
Zan Zibert <zan_zibert@hotmail.com>
2020-07-16 13:21:12 +0200

    much simpler solution for best time to buy stock II

--- leetcode/122_Best_Time_to_Buy_and_Sell_Stock_II/main.go
+++ leetcode/122_Best_Time_to_Buy_and_Sell_Stock_II/main.go
@@ -1,26 +1,12 @@
 package main
 
 func maxProfit(prices []int) int {
-	valley := prices[0]
-	peak := prices[0]
 	maxSum := 0
 
 	for i := 1; i < len(prices); i++ {
-		if prices[i] < valley {
-			maxSum += (peak - valley)
-			valley = prices[i]
-			peak = prices[i]
-		} else if prices[i] > peak {
-			peak = prices[i]
-		} else if prices[i] < peak {
-			maxSum += (peak - valley)
-			peak = prices[i]
-			valley = prices[i]
+		if prices[i] > prices[i-1] {
+			maxSum += (prices[i] - prices[i-1])
 		}
-	}
-
-	if peak > valley {
-		maxSum += (peak - valley)
 	}
 
 	return maxSum
