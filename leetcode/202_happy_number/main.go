05fb4a6e1570fdfe8e0bdebd91b2735ecf308f77
Zan Zibert <zan_zibert@hotmail.com>
2020-07-18 12:41:38 +0200

    completed 202 happy number, naive approach

--- leetcode/202_happy_number/main.go
+++ leetcode/202_happy_number/main.go
@@ -1,5 +1,39 @@
 package main
 
-func isHappy(n int) bool {
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	fmt.Println(isHappy(19))
+}
+
+func isHappy(n int) bool {
+	numbers := make(map[int]bool)
+
+	for n != 1 {
+		Itoa := strconv.Itoa(n)
+		digits := strings.Split(Itoa, "")
+		number := sumOfSquares(digits)
+		if numbers[number] {
+			return false
+		}
+		numbers[number] = true
+		n = number
+	}
+
+	return true
+}
+
+func sumOfSquares(numbers []string) int {
+	sum := 0
+
+	for _, str := range numbers {
+		number, _ := strconv.Atoi(str)
+		sum += number * number
+	}
+
+	return sum
 }
