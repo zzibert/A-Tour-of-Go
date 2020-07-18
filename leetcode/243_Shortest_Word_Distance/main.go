180aa866744039793f91922793aa897e46fe28d1
Zan Zibert <zan_zibert@hotmail.com>
2020-07-18 08:22:58 +0200

    completed 243 shortest word in linear time O(n)

--- leetcode/243_Shortest_Word_Distance/main.go
+++ leetcode/243_Shortest_Word_Distance/main.go
@@ -1,39 +1,23 @@
 package main
 
 func shortestDistance(words []string, word1 string, word2 string) int {
-	var index1, index2 int
 	distance := len(words)
+
+	index1, index2 := -1, -1
 
 	for i, word := range words {
 		if word == word1 {
 			index1 = i
-			index2 = findWord(words[i+1:], word2)
-			if index2 == 0 {
-				break
+			if index2 != -1 && index1-index2 < distance {
+				distance = index1 - index2
 			}
-			if index2 < distance {
-				distance = index2
-			}
-		}
-		if word == word2 {
+		} else if word == word2 {
 			index2 = i
-			index1 = findWord(words[i+1:], word1)
-			if index1 == 0 {
-				break
-			}
-			if index1 < distance {
-				distance = index1
+			if index1 != -1 && index2-index1 < distance {
+				distance = index2 - index1
 			}
 		}
 	}
-	return distance
-}
 
-func findWord(words []string, word string) int {
-	for i, val := range words {
-		if val == word {
-			return i + 1
-		}
-	}
-	return 0
+	return distance
 }
