f61851b797947c46a58ae3d26aba94c5ee9a0690
Zan Zibert <zan_zibert@hotmail.com>
2020-07-18 20:28:51 +0200

    completed 88 merge sorted array, naive approach

--- leetcode/53_Maximum_Subarray/main.go
+++ leetcode/53_Maximum_Subarray/main.go
@@ -5,6 +5,5 @@ func main() {
 }
 
 func maxSubArray(nums []int) int {
-	maxSum := 0
 
 }
--- leetcode/88_merge_sorted_array.go/naive.go
+++ leetcode/88_merge_sorted_array.go/naive.go
@@ -1,25 +1,48 @@
 package main
 
-func merge(nums1 []int, m int, nums2 []int, n int) {
-	var index int
+import "fmt"
 
-	for len(nums2) != 0 && index < len(nums1) {
-		if nums1[index] > nums2[0] {
-			pushForward(nums1, index, nums2[0])
-			nums2 = nums2[1:]
-		} else {
-			index++
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+
+	merge(nums1, 3, nums2, 3)
+
+	fmt.Println(nums1)
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if len(nums2) == 0 {
+		return
+	}
+	nums1 = nums1[:m]
+
+	for i := 0; i < m; i++ {
+		if nums2[0] < nums1[i] {
+			temp := nums1[i]
+			nums1[i] = nums2[0]
+			nums2[0] = temp
+			sort(nums2)
 		}
 	}
 
-	nums1 = append(nums1, nums2...)
+	for _, number := range nums2 {
+		nums1 = append(nums1, number)
+	}
 }
 
-func pushForward(nums []int, index int, val int) {
-	temp := nums[index+1]
-	for i := index; i < len(nums)-1; i++ {
-		nums[i] = val
-		val = temp
-		temp = nums[i+1]
+func sort(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		min := nums[i]
+		index := i
+		for j := i + 1; j < len(nums); j++ {
+			if min > nums[j] {
+				min = nums[j]
+				index = j
+			}
+		}
+		temp := nums[i]
+		nums[i] = min
+		nums[index] = temp
 	}
 }
