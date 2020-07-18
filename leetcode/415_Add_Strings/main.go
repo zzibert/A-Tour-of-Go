6b8092bbfea7e87151e26c767f22c89c15bea321
Zan Zibert <zan_zibert@hotmail.com>
2020-07-18 13:26:21 +0200

    completed 415 add strings, naive approach

--- leetcode/415_Add_Strings/main.go
+++ leetcode/415_Add_Strings/main.go
@@ -1,6 +1,68 @@
 package main
 
-func addStrings(num1 string, num2 string) string {
+import (
+	"strconv"
+	"strings"
+)
 
-  
+func addStrings(num1 string, num2 string) string {
+	digits1 := Atoi(num1)
+	digits2 := Atoi(num2)
+
+	length1 := len(digits1)
+	length2 := len(digits2)
+
+	digits1 = append(make([]int, 5100-length1), digits1...)
+	digits2 = append(make([]int, 5100-length2), digits2...)
+
+	for i := 0; i < 5100; i++ {
+		digits2[i] = digits2[i] + digits1[i]
+	}
+
+	// summing up the digits of the two numbers.
+	for i := len(digits2) - 1; i >= 0; i-- {
+		if digits2[i] > 9 {
+			digits2[i] -= 10
+			digits2[i-1] += 1
+		}
+	}
+	return Itoa(digits2)
+}
+
+// Converting the array of digits back into number string.
+func Itoa(digits []int) string {
+	numbers := make([]string, 0)
+
+	index := removeZeroes(digits)
+
+	if index == len(digits) {
+		return "0"
+	}
+
+	for _, digit := range digits[index:] {
+		numbers = append(numbers, strconv.Itoa(digit))
+	}
+	return strings.Join(numbers, "")
+}
+
+// converting the number string into a array of digits.
+func Atoi(number string) []int {
+	digits := strings.Split(number, "")
+	numbers := make([]int, 0)
+
+	for _, digit := range digits {
+		number, _ := strconv.Atoi(digit)
+		numbers = append(numbers, number)
+	}
+	return numbers
+}
+
+// Remove redundant zeroes
+func removeZeroes(digits []int) int {
+	for i, digit := range digits {
+		if digit != 0 {
+			return i
+		}
+	}
+	return len(digits)
 }
