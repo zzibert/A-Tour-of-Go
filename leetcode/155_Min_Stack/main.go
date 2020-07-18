681c8b2e5297363001f18fd656824f8fff536af9
Zan Zibert <zan_zibert@hotmail.com>
2020-07-18 14:17:20 +0200

    155 min stack with getMin of O(1)

--- leetcode/155_Min_Stack/main.go
+++ leetcode/155_Min_Stack/main.go
@@ -1,8 +1,12 @@
 package main
 
+type Number struct {
+	val int
+	min int
+}
+
 type MinStack struct {
-	stack  []int
-	min    int
+	stack  []Number
 	length int
 }
 
@@ -11,17 +15,16 @@ const MinInt = -MaxInt - 1
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
-	return MinStack{min: MinInt}
+	return MinStack{}
 }
 
 func (this *MinStack) Push(x int) {
-	if len(this.stack) == 0 {
-		this.min = x
-	} else if x < this.min {
-		this.min = x
+	number := Number{val: x, min: x}
+	if this.length > 0 && this.stack[this.length-1].min < x {
+		number.min = this.stack[this.length-1].min
 	}
 
-	this.stack = append(this.stack, x)
+	this.stack = append(this.stack, number)
 	this.length++
 }
 
@@ -31,7 +34,6 @@ func (this *MinStack) Pop() {
 	}
 
 	this.stack = this.stack[:this.length-1]
-	this.min = findMin(this.stack)
 	this.length--
 }
 
@@ -39,21 +41,11 @@ func (this *MinStack) Top() int {
 	if this.length == 0 {
 		return 0
 	}
-	return this.stack[this.length-1]
+	return this.stack[this.length-1].val
 }
 
 func (this *MinStack) GetMin() int {
-	return this.min
-}
-
-func findMin(stack []int) int {
-	min := MaxInt
-	for _, number := range stack {
-		if number < min {
-			min = number
-		}
-	}
-	return min
+	return this.stack[this.length-1].min
 }
 
 /**
