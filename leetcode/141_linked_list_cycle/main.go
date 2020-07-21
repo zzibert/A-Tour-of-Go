5613ad27ec2eb23dadff08985cb3c600b8d177b6
Zan Zibert <zan_zibert@hotmail.com>
2020-07-21 14:37:43 +0200

    completed 141 linked list cycle

--- leetcode/141_linked_list_cycle/main.go
+++ leetcode/141_linked_list_cycle/main.go
@@ -1,7 +1,27 @@
 package main
 
-func main() {
-
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
 
+	slow := head
+	fast := head.Next.Next
+
+	for fast != nil {
+		if slow == fast {
+			return true
+		}
+		slow = slow.Next
+		if fast.Next == nil {
+			return false
+		}
+		fast = fast.Next.Next
+	}
+	return false
+}
