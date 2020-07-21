7925428d24a83561f8b8a07e9b549f544f5ef990
Zan Zibert <zan_zibert@hotmail.com>
2020-07-21 12:32:11 +0200

    max consecutive ones II completed

--- master-the-coding-interview/arrays/max_consecutive_ones.go
+++ master-the-coding-interview/arrays/max_consecutive_ones.go
@@ -1,24 +1,52 @@
 package main
 
 func findMaxConsecutiveOnes(nums []int) int {
-	max := 0
-	index := 0
 
-	for index < len(nums) {
-		if nums[index] == 1 {
-			localMax := 0
-			localIndex := index
-			for localIndex < len(nums) && nums[localIndex] == 1 {
-				localMax++
-				localIndex++
-			}
-			if localMax > max {
-				max = localMax
-			}
-			index = localIndex
-		} else {
-			index++
+	max := 1
+
+	zeroes := findZeroes(nums)
+
+	if len(zeroes) == 0 {
+		return len(nums)
+	}
+
+	for _, zero := range zeroes {
+		localMax := countOnes(nums, zero)
+		if localMax > max {
+			max = localMax
 		}
 	}
+
 	return max
+}
+
+func findZeroes(nums []int) []int {
+	array := make([]int, 0)
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			array = append(array, i)
+		}
+	}
+	return array
+}
+
+func countOnes(nums []int, index int) int {
+	count := 1
+	left := index - 1
+	right := index + 1
+
+	// count ones before flipped zero
+	for left >= 0 && nums[left] == 1 {
+		count++
+		left--
+	}
+
+	// count ones after flipped zero
+	for right < len(nums) && nums[right] == 1 {
+		count++
+		right++
+	}
+
+	return count
 }
