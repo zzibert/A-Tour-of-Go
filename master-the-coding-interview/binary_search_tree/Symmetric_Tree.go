b1c22ddbdc66d69ba4ba22827b3041285598d3ba
Zan Zibert <zan_zibert@hotmail.com>
2020-07-23 13:30:30 +0200

    completed symmetric tree again

---
+++ master-the-coding-interview/binary_search_tree/Symmetric_Tree.go
@@ -0,0 +1,21 @@
+package main
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return isSymmetricHelper(root.Left, root.Right)
+}
+
+func isSymmetricHelper(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil || left.Val != right.Val {
+		return false
+	}
+
+	return isSymmetricHelper(left.Left, right.Right) && isSymmetricHelper(left.Right, right.Left)
+}
