b5e5afc86ad8afcc3be0e835c15c2e42002d8b0a
Zan Zibert <zan_zibert@hotmail.com>
2020-07-26 12:28:59 +0200

    completed contains duplicate II

---
+++ master-the-coding-interview/hash_tables/Contains_Duplicate_II.go
@@ -0,0 +1,22 @@
+package main
+
+import "math"
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	elements := make(map[int]int)
+
+	if len(nums) == 0 {
+		return false
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if _, ok := elements[nums[i]]; ok {
+			if math.Abs(float64(elements[nums[i]])-float64(i)) <= float64(k) {
+				return true
+			}
+		}
+		elements[nums[i]] = i
+	}
+
+	return false
+}
