472856e78425168328bfe427232b5d5577f99cd8
Zan Zibert <zan_zibert@hotmail.com>
2020-08-31 09:40:11 +0200

    completed 561 array partition

--- leetcode/561_Array_Partition_I/main.go
+++ leetcode/561_Array_Partition_I/main.go
@@ -2,11 +2,32 @@ package main
 
 func arrayPairSum(nums []int) int {
 
-	values := make(map[int]int)
+	nums = sort(nums)
 
+	sum := 0
+
+	for i := 0; i < len(nums); i += 2 {
+		sum += nums[i]
+	}
+
+	return sum
+
+}
+
+func sort(nums []int) []int {
 	for i := 0; i < len(nums); i++ {
-		values[nums[i]] += 1
-  }
-  
-  for 
+		min := nums[i]
+		index := i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < min {
+				min = nums[j]
+				index = j
+			}
+		}
+		temp := nums[i]
+		nums[i] = min
+		nums[index] = temp
+	}
+
+	return nums
 }
